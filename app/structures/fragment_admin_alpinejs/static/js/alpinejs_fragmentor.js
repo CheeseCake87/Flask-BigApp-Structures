c7e5a86201b040c640bdffffef8b7f849195320b
David Carmichael <carmichaelits@gmail.com>
2022-11-23 14:31:10 +0000

    added time delay to fetcher

--- app/structures/fragment_admin_alpinejs/static/js/alpinejs_fragmentor.js
+++ app/structures/fragment_admin_alpinejs/static/js/alpinejs_fragmentor.js
@@ -1,12 +1,15 @@
 document.addEventListener('alpine:init', () => {
 
     Alpine.store('fragmentor', {
-        async get(url, inject_el) {
+        async get(url, inject_el, wait = 0) {
+            console.log("get: " + url + " inject_el: " + inject_el)
             const fetcher = await fetch(url).catch(error => {
                 console.log(error);
             });
             if (fetcher.status === 200) {
-                inject_el.innerHTML = await fetcher.text();
+                setTimeout(async () => {
+                    inject_el.innerHTML = await fetcher.text();
+                }, wait);
             }
         }
     });
