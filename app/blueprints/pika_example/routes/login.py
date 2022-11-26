ff64d54a199eea06de14e22a8a52ed7762332008
David Carmichael <carmichaelits@gmail.com>
2022-11-26 22:43:04 +0000

    Small changes

--- app/blueprints/pika_example/routes/login.py
+++ app/blueprints/pika_example/routes/login.py
@@ -1,8 +1,16 @@
 from flask import render_template
 
+from mmforms.standard_elements import Form
+from mmforms.bootstrap_elements import Input
+
 from .. import bp, page_needs
 
 
 @bp.route("/login", methods=["GET"])
 def login():
-    return render_template(bp.tmpl("login.html"), **page_needs)
+    login_form = Form("login_form").add_inputs(
+        Input("username").t_text(),
+        Input("password").t_password()
+    )
+
+    return render_template(bp.tmpl("login.html"), **page_needs, login_form=login_form.compile())
